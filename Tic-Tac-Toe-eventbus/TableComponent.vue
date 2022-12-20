79e2dd155934a4d83b4ad77e262e23f80d1db7f4
JwaSangHeon <whk4051@naver.com>
2022-12-20 16:30:10 +0900

    EventBus 이용한 틱택토

---
+++ Tic-Tac-Toe-eventbus/TableComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <table>
+    <tr-component
+      v-for="(rowData, index) in tableData"
+      :rowData="rowData"
+      :rowIndex="index"
+      :key="index"
+    ></tr-component>
+  </table>
+</template>
+
+<script>
+import TrComponent from "./TrComponent.vue";
+
+export default {
+  components: { TrComponent },
+
+  props: {
+    tableData: Array,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ Tic-Tac-Toe-eventbus/TrComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <tr>
+    <td-component
+      v-for="(cellData, index) in rowData"
+      :cellData="cellData"
+      :rowIndex="rowIndex"
+      :cellIndex="index"
+      :key="index"
+    ></td-component>
+  </tr>
+</template>
+
+<script>
+import TdComponent from "./TdComponent.vue";
+
+export default {
+  props: {
+    rowData: Array,
+    rowIndex: Number,
+  },
+  data() {
+    return {
+      parent: "parent",
+    };
+  },
+  components: {
+    TdComponent,
+  },
+};
+</script>
